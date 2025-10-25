02ec681d499eac738781f43a28151ef86a7f607c
tristan <tristan.alhabas@gmail.com>
2025-10-25 17:57:41 +0700

    feat: Quiz Page Placeholder

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,15 @@ import App from "./App";
 import './index.css';
 import SignIn from "./pages/signin/SignIn";
 import SignUp from "./pages/signup/SignUp";
+import Quiz from "./pages/quiz/Quiz";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
         <Routes>
             <Route path="*" element={<App />} />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/quiz" element={<Quiz />} />
         </Routes>
     </BrowserRouter>
 );
